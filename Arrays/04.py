d90e02797c8512dd1f208e18c407c0bdf6915743
Manish Gupta <166051072+ManishGx@users.noreply.github.com>
2025-10-26 21:33:33 +0530

    Add files via upload

---
+++ Arrays/04.py
@@ -0,0 +1,21 @@
+# Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+# Note: You need to solve this problem without utilizing the built-in sort function.
+
+def sort_012(arr):
+    low = 0
+    mid = 0 
+    high = len(arr) -1
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
+    return arr
+
+arr = [0, 1, 2, 0, 1, 2]
+print(sort_012(arr))
